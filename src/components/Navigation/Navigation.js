1267264f60dcef2a738139274c5e97ff803e27a9
Chris Carlson <cecarlson@gmail.com>
2019-07-20 16:55:58 -0500

    moved add task and add category out of navigation and into page

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,7 +5,7 @@ import './Navigation.css'
 export default class Navigation extends Component {
   render() {
       return <>
-          <p><Link to='/home'>Home</Link> | <Link to='/addnewtask'>+ Add New Task</Link> | <Link to='/addnewcategory'>+ Add New Category</Link></p>
+          <p><Link to='/home'>Home</Link></p>
     </>
   }
 }
