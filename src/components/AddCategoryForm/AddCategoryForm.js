5944bac81a441661ee444e5cc105a93fa27a3b23
Chris Carlson <cecarlson@gmail.com>
2019-07-20 16:53:33 -0500

    added now.json and made minor UI changes to add category form

--- src/components/AddCategoryForm/AddCategoryForm.js
+++ src/components/AddCategoryForm/AddCategoryForm.js
@@ -4,16 +4,20 @@ import './AddCategoryForm.css'
 export default class AddCategoryForm extends Component {
   render() {
       return <>
+          <section>
+          <header>
+            <h2>Add New Category</h2>
+        </header>
          <form id="add-new-category">
           <div className="form-section">
-            <h3>Add a New Category</h3>
             <label htmlFor="category-name">Category Name:</label>
             <input type="text" id="category-name" name="category-name" placeholder="Personal" required />
           </div>
           <div className="form-section">
             <button type="submit">Add Category</button>
           </div>
-          </form>
+              </form>
+              </section>
     </>
   }
 }
