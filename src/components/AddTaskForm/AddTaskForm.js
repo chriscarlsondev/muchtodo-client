4bf0de72a6232da5078583089c7d841554d678b3
Chris Carlson <cecarlson@gmail.com>
2019-07-20 16:38:47 -0500

    added stub code for add task form

---
+++ src/components/AddTaskForm/AddTaskForm.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import './AddTaskForm.css'
+import { Link } from 'react-router-dom'
+
+export default class AddTaskForm extends Component {
+  render() {
+      return <>
+         <section>
+        <header>
+            <h2>Add New Task</h2>
+        </header>
+         <form id="add-new-task">
+          <div className="form-section">
+            <h3>Task Name</h3>
+            <input type="text" name="task-title" placeholder="Schedule a doctor's appointment" required />
+          </div>
+          <div className="form-section">
+            <h3>Task Categories (Optional)</h3>
+            <input type="checkbox" name="categories" id="cat1" />
+            <label htmlFor="cat1">Category 1</label><br />
+            <input type="checkbox" name="categories" id="cat2" />
+            <label htmlFor="cat2">Category 2</label><br />
+            <Link to='/addnewcategory'>+ Add New Category</Link>
+          </div>
+          <div className="form-section">
+            <h3>Due Date (Optional)</h3>
+            <label htmlFor="task-due-date">Due Date:</label>
+            <input type="date" name="task-due-date" id="task-due-date" />
+          </div>
+          <div className="form-section">
+            <button type="submit">Add Task</button>
+          </div>
+          </form>
+      </section>
+    </>
+  }
+}
