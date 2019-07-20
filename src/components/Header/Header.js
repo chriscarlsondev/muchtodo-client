336aa8aa44cd421b0a71b3b8d0bf87f79a332c80
Chris Carlson <cecarlson@gmail.com>
2019-07-20 15:59:03 -0500

    updated css to move to proper component css file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ export default class Header extends Component {
   render() {
     return <>
         <h1>MuchToDo</h1>
-        <h2>Spend less time tracking your to do's and more time getting them done.</h2>
+        <p>Spend less time tracking your to do's and more time getting them done.</p>
     </>
   }
 }
