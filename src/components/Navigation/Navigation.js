1c255a3598bc2920ac2dff97b5d21aec7b6ba70c
Chris Carlson <cecarlson@gmail.com>
2019-07-22 07:34:39 -0500

    updated navigation links

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,7 +5,8 @@ import './Navigation.css'
 export default class Navigation extends Component {
   render() {
       return <>
-          <p><Link to='/home'>Home</Link></p>
+        <p>
+          <Link to='/'>Intro</Link> &#8226; <Link to='/home'>Home</Link> &#8226; <Link to='/addnewtask'>Add Task</Link> &#8226; <Link to='/addnewcategory'>Add Category</Link></p>
     </>
   }
 }
