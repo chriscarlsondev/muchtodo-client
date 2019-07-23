89e9379b9611f9543c4be6e00efb69a1ca064db7
Chris Carlson <cecarlson@gmail.com>
2019-07-23 13:31:12 -0500

    added image logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import logo from './muchtodo-logo.png'
 
 export default class Header extends Component {
   render() {
@@ -9,7 +10,7 @@ export default class Header extends Component {
         <Link to='/home'>Home</Link> &bull; <Link to='/addnewtask'>Add Task</Link> &bull; <Link to='/addnewcategory'>Add Category</Link> &bull; <Link to="/">About</Link>
       </nav>
       <header role="banner">
-        <Link to="/home" className="logo-link"><h1><span className="logo">&#10003;</span>MuchToDo</h1></Link>
+        <Link to="/home" className="logo-link"><img src={logo} alt="MuchToDo" className="logo" /></Link>
         <span>Spend less time tracking your to do's and more time getting them done.</span>
         </header>
       </>
